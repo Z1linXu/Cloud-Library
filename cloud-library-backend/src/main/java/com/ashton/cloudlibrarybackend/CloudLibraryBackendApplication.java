6ec8aa69d86d622084bd3382358d300aaaaf0a73
Zilin <zxu722@gatech.edu>
2025-11-30 14:02:50 -0500

    chore: set up base project with Spring Boot 2.7 and essential dependencies

--- cloud-library-backend/src/main/java/com/ashton/cloudlibrarybackend/CloudLibraryBackendApplication.java
+++ cloud-library-backend/src/main/java/com/ashton/cloudlibrarybackend/CloudLibraryBackendApplication.java
@@ -3,9 +3,11 @@ package com.ashton.cloudlibrarybackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.ashton.cloudlibrarybackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class CloudLibraryBackendApplication {
 
     public static void main(String[] args) {

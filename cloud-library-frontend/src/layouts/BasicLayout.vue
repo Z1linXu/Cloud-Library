fe21cf99f0dfe5c793e209127afdf8bbb3e9c7f3
Zilin <zxu722@gatech.edu>
2025-12-05 17:25:48 -0500

    feat: add global header component with navigation and login button

---
+++ cloud-library-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="basic-layout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://www.zilinxu.com" target="_blank"> @2025 Zilin Xu </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+<style scoped>
+
+#basic-layout .header {
+  padding-inline: 20px;
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+}
+
+#basic-layout .content {
+  padding: 20px;
+  background: linear-gradient(to right, #efefef, #fff);
+  margin-bottom: 28px;
+}
+
+#basic-layout .footer{
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+
+}
+
+
+</style>
